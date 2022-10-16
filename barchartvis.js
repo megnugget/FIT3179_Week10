b350f80b7909e8d3c88fe9750c31d79781a3748c
megnugget <114331457+megnugget@users.noreply.github.com>
2022-10-16 14:49:22 +0800

    Add files via upload

---
+++ barchartvis.js
@@ -0,0 +1,4 @@
+var vg_barchart = "barchartw10.json";
+vegaEmbed('#bar_chart',vg_barchart).then(function(result){
+
+}).catch(console.error);
---
+++ vega_lite_vis.js
@@ -0,0 +1,4 @@
+var vg_map = "mapw9.json";
+vegaEmbed('#choropleth_map',vg_map).then(function(result){
+
+}).catch(console.error);
